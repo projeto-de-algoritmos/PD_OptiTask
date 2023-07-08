a5a50c8f4c09486f338e086fb1bae9d4eeb5baeb
kayrocesar <kayro.cesar.kc@gmail.com>
2023-07-08 18:23:50 -0300

    acrescentando rota de calculo e conversao de data
    Co-Authored-By: Eduardo Maia <emaiarezende@gmail.com>

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 
-import datetime
+from datetime import datetime
 from flask import Flask, render_template, request, jsonify
 from algorithm import  *
 
@@ -16,19 +16,68 @@ def index():
 @app.route('/submit', methods=['POST'])
 def process_data():
     data = request.json  # Obtendo os dados JSON do corpo da requisição
+    cont= data.get('contador')
     texto1 = data.get('valorInput')
     data1 = data.get('valorInput2')
     data2 = data.get('valorInput3')
     texto2 = data.get('valorInput4')
 
-    tarefas.append(Tarefa(texto1, data1, data2, texto2))
+    tarefas.append(Tarefa(cont,texto1, convert_data_ms(data1), convert_data_ms(data2), texto2)) # Adiciona a tarefa na lista de tarefas
 
+    #tarefas.append(Tarefa(cont,texto1, data1, data2, texto2)) # Adiciona a tarefa na lista de tarefas
+    
+   
     for t in tarefas:  # teste
-        print('Nome da tarefa ' ,t.nome, t.inicio, t.fim, t.lucro) # Imprime a lista de tarefas
+        print('Nome da tarefa ' ,t.ident, t.nome, t.inicio, t.fim, t.lucro) # Imprime a lista de tarefas
 
     return jsonify({'valorInput': texto1, 'valorInput2': data1, 'valorInput3': data2, 'valorInput4': texto2})
 
+
+
+
+@app.route('/calc', methods=['GET'])
+def obter_tarefas():
+    lucro_otimo, tarefas_realizadas = escalonamento(tarefas)
+
+    print("Lucro ótimo é:", lucro_otimo)
+    print("Tarefas a serem realizadas:")
+    for tarefa in tarefas_realizadas:
+       print("Nome:", tarefa.nome, "Início:", tarefa.inicio, "Fim:", tarefa.fim, "Lucro:", tarefa.lucro)
     
+    # Converter a lista de tarefas em uma lista de dicionários
+    tarefas_serializaveis = [tarefa.to_dict() for tarefa in tarefas_realizadas]
+
+    # Retornar a lista de tarefas em formato JSON
+    return jsonify(tarefas=tarefas_serializaveis, lucro_otimo=lucro_otimo)
+
+
+'''
+@app.route('/delete/<int:id>', methods=['GET'])
+def deleter_tarefa(id):
+    for tarefa in tarefas:
+        if tarefa.ident == id:
+            tarefas.remove(tarefa)
+            return jsonify({'message': 'Item removido com sucesso'})
+    return jsonify({'message': 'Item não encontrado'})
+
+    print("Tarefas:")
+    for i in tarefas:
+       print("Nome:", i.nome, "Início:", i.inicio, "Fim:", i.fim, "Lucro:", i.lucro)
+    # Retornar a lista de tarefas em formato JSON
+    #return jsonify(tarefas=tarefas_serializaveis, lucro_otimo=lucro_otimo)
+
+
+'''
+
+
+def convert_data_ms(dataInput):
+     # Formata a data do formulário em um objeto datetime
+    data_f = datetime.strptime(dataInput, "%Y-%m-%d")
+    dataMili = int (datetime.timestamp(data_f) * 1000)  # Converte a data para milissegundos
+    print( dataMili)
+    return dataMili
+
+
 if __name__ == '__main__':
     app.run(debug=True)
     
