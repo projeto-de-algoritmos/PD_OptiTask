57370fffc8aae1669c0ed23396e2a6a8d4d94bc8
kayrocesar <kayro.cesar.kc@gmail.com>
2023-07-09 20:08:23 -0300

    add formatação de datas, exibição do resultado e deletar tarefas
    Co-Authored-By: Eduardo Maia <emaiarezende@gmail.com>

--- src/static/js/script.js
+++ src/static/js/script.js
@@ -5,34 +5,65 @@ let dataFim = document.getElementById("inputTarefa3");
 let preco = document.getElementById("inputTarefa4");
 let btnAdd = document.getElementById("btn-add");
 let main = document.getElementById("areaLista");
-
-
+let lucroMaximoElement = document.createElement("div");
 
 
 function verificarLucroMax() {
     document.getElementById("submit-button").addEventListener("click", function () {
         fetch('/calc')
             .then(response => response.json())
-            .then(tarefas => {
-                // Exibir o resultado no console
-                console.log(tarefas);
+            .then(data => {
+                const cardsContainer = document.getElementById("resultado"); // Criar um card para cada elemento do JSON recebido
+                cardsContainer.innerHTML = ''; 
+                lucroMaximoElement.innerHTML = ''; 
+                lucroMaximoElement.classList.add("lucro-maximo");
+                lucroMaximoElement.textContent = `Lucro máximo: ${data.lucro_total}`;
+                cardsContainer.appendChild(lucroMaximoElement);
+
+                data.tarefas.forEach(tarefa => {
+
+                    const card = document.createElement("div");
+                    card.classList.add("card");
+                    card.innerHTML = ` 
+                    <h3>${tarefa.nome}</h3>
+                    <p>Início: ${tarefa.inicio}</p>
+                    <p>Fim: ${tarefa.fim}</p>
+                    <p>Lucro: ${tarefa.lucro}</p>
+                `;
+                    cardsContainer.appendChild(card);
+                });
+
             })
             .catch(error => {
                 console.error(error);
             });
     });
 }
+
+//Converte formato da data para o usuario
+function convertDateFormat(dateString) {
+    var dateParts = dateString.split("-");
+    var year = dateParts[0];
+    var month = dateParts[1];
+    var day = dateParts[2];
+    var formattedDate = day + "/" + month + "/" + year;
+    return formattedDate;
+  }
+  
 // Adicionar o evento de clique ao botão assim que a página for carregada
 document.addEventListener('DOMContentLoaded', verificarLucroMax);
 
 function addTarefa() {
-    //PEGAR O VALOR DIGITADO NO descTarefa
+    const cardsContainer = document.getElementById("resultado");
+    cardsContainer.innerHTML = ''; 
+    lucroMaximoElement.innerHTML = ''; 
+
     let valorInput = descTarefa.value;
-    let valorInput2 = dataInicio.value;
-    let valorInput3 = dataFim.value;
+    let valorInput2 =  dataInicio.value;
+    let valorInput3 =  dataFim.value;
     let valorInput4 = preco.value;
-
-
+    
+    
     // verificar se o valor é válido em algum dos casos
     if (valorInput != "" || valorInput2 != "" || valorInput3 != "" || valorInput4 != "") {
         ++contador;
@@ -43,7 +74,7 @@ function addTarefa() {
                 </div>
 
                 <div onclick="marcarTarefa(${contador})" class="item-nome">
-                    ${valorInput}   ${valorInput2}  ${valorInput3} ${valorInput4}
+                  Tarefa: <span class="sem-negrito"> ${valorInput} </span> <br> Início: <span class="sem-negrito"> ${convertDateFormat(valorInput2)}</span> <br> Fim: <span class="sem-negrito"> ${convertDateFormat(valorInput3)}</span>  <br> Valor: <span class="sem-negrito"> R$ ${valorInput4} </span>
                 </div>
                 
                 <div class="item-botao">
@@ -54,15 +85,13 @@ function addTarefa() {
         // inserir o novo item na lista
         main.innerHTML += novoItem;
 
-        //zerar o input
+        //zerar os inputs
         descTarefa.value = "";
         dataInicio.value = "";
         dataFim.value = "";
         preco.value = "";
 
-
-
-
+        // objeto que representa cada tarefa adicionada com seus atributos
         const dados = {
             contador: contador,
             valorInput: valorInput,
@@ -80,8 +109,8 @@ function addTarefa() {
         })
             .then(response => response.json()) // Obtém a resposta como objeto JSON
             .then(data => {
-                // Manipulação dos dados recebidos
-                console.log(data); // Resposta recebida do backend
+
+                console.log(data); // Resposta recebida do backend (Informações da tarefa adicionada)
             })
             .catch(error => {
                 // Tratamento de erros
@@ -90,25 +119,25 @@ function addTarefa() {
     }
 }
 
-/*
+
 function deletar(id) {
-            var tarefa = document.getElementById(id);
-            tarefa.remove();
+    const cardsContainer = document.getElementById("resultado");
+    lucroMaximoElement.innerHTML = ''; 
+    var tarefa = document.getElementById(id);  // Pegar o elemento HTML da tarefa que será removida
+    tarefa.remove();
+    fetch('/delete/' + id, {
+        method: 'DELETE'
+    })
+        .then(response => response.json())
+        .then(data => {
+            // Exibir se a tarefa foi removida com sucesso
+            console.log(data);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+}
 
-            document.getElementById("deletar").addEventListener("click", function () {
-                fetch('/delete/${id}')
-                    .then(response => response.json())
-                    .then(tarefas => {
-                        // Exibir o resultado no console
-                        console.log(tarefas);
-                    })
-                    .catch(error => {
-                        console.error(error);
-                    });
-            });
-
-        }
-*/
 function marcarTarefa(id) {
     var item = document.getElementById(id);
     var classe = item.getAttribute("class");
